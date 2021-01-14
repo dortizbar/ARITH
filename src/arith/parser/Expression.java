7f6a8744b239b817da6557ec963caac5fd6c7774
Diego O <dortizba@ucsc.edu>
2021-01-13 22:08:53 -0500

    modifying parse object

--- src/arith/parser/Expression.java
+++ src/arith/parser/Expression.java
@@ -1,5 +1,36 @@
 package arith.parser;
 
+
+import java.util.HashSet;
+
 public class Expression {
 
+	private final HashSet<String> numbers = new HashSet<>();
+	
+	private final HashSet<String> symbols  = new HashSet<>();;
+
+	public HashSet<String> getNumbers() {
+		return numbers;
+	}
+
+	
+	public HashSet<String> getSymbols() {
+		return symbols;
+	}
+	
+	public Expression number(final String num) {
+		numbers.add(num);
+		return this;
+	}
+	
+	public Expression symbol(final String sym) {
+		symbols.add(sym);
+		return this;
+	}
+
+	
+
+	
+	
+	
 }
